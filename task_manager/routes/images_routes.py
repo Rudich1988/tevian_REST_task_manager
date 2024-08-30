70e94e7b187b12290801f2057eb1a7ed500f8d62
rudich <rudi4.clarinet@mail.ru>
2024-08-30 16:02:34 +0400

    correct linter images_routes

--- task_manager/routes/images_routes.py
+++ task_manager/routes/images_routes.py
@@ -10,57 +10,65 @@ images_bp = Blueprint('images_routes', __name__)
 
 
 @images_bp.route('/images/<int:id>', methods=['GET'])
-def get_image(id: int,
-              image_service=ImageService,
-              image_repo=ImageRepository,
-              image_schema=ImageSchemaAdd,
-              session=Session
-              ):
+def get_image(
+        id: int,
+        image_service=ImageService,
+        image_repo=ImageRepository,
+        image_schema=ImageSchemaAdd,
+        session=Session
+):
     try:
-        image = image_service(image_repo=image_repo,
-                              session=session()
-                              ).get_image(image_data={'id': id},
-                                          schema=image_schema
-                                          )
+        image = image_service(
+            image_repo=image_repo,
+            session=session()).get_image(
+            image_data={'id': id},
+            schema=image_schema
+        )
     except:
-        return make_response(jsonify({'error': 'Error get image'}),
-                             404
-                             )
+        return make_response(
+            jsonify({'error': 'Error get image'}),
+            404
+        )
     return jsonify(image)
 
 
 @images_bp.route('/images', methods=['POST'])
-def create_image(image_service=ImageService,
-                 image_repo=ImageRepository,
-                 image_schema=ImageSchemaAdd,
-                 session=Session
-                 ):
+def create_image(
+        image_service=ImageService,
+        image_repo=ImageRepository,
+        image_schema=ImageSchemaAdd,
+        session=Session
+):
     try:
         image_data = request.json
-        image = image_service(image_repo=image_repo,
-                              session=session()
-                              ).add_image(image_data=image_data,
-                                          schema=image_schema
-                                          )
+        image = image_service(
+            image_repo=image_repo,
+            session=session()).add_image(
+            image_data=image_data,
+            schema=image_schema
+        )
     except:
-        return make_response(jsonify({'error': 'Error create task'}),
-                             404
-                             )
+        return make_response(
+            jsonify({'error': 'Error create task'}),
+            404
+        )
     return jsonify(image)
 
 
 @images_bp.route('/images/<int:id>', methods=['DELETE'])
-def delete_image(id: int,
-                 image_service=ImageService,
-                 image_repo=ImageRepository,
-                 session=Session
-                 ):
+def delete_image(
+        id: int,
+        image_service=ImageService,
+        image_repo=ImageRepository,
+        session=Session
+):
     try:
-        response = image_service(image_repo=image_repo,
-                                 session=session()
-                                 ).delete_image({'id': id})
+        response = image_service(
+            image_repo=image_repo,
+            session=session()).delete_image({'id': id})
     except:
-        return make_response(jsonify({'error': 'Error delete image'}),
-                             404
-                             )
+        return make_response(
+            jsonify({'error': 'Error delete image'}),
+            404
+        )
     return make_response(jsonify(response))
