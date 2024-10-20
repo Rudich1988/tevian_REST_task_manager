a9f7df66dc11dc1548e03b8a3e8ab432a9c144a8
rudich <rudi4.clarinet@mail.ru>
2024-10-20 17:03:39 +0400

    correct utils repo

--- task_manager/utils/repository.py
+++ task_manager/utils/repository.py
@@ -30,34 +30,11 @@ class SQLAlchemyRepository(AbstractRepository):
     def __init__(self, session: Session):
         self.session = session
 
-    def add_one(self, data: dict):
-        obj = self.model(**data)
-        self.session.add(obj)
-        self.session.flush()
-        return obj
+    def add_one(self, object_id: int):
+        pass
 
-    def add_objects(self, data):
-        objects = [self.model(**item) for item in data]
-        self.session.add_all(objects)
-        self.session.flush()
-        return [obj.id for obj in objects]
+    def get_one(self, object_id: int):
+        pass
 
-    def get_one(self, data: dict):
-        obj = self.session.query(self.model).filter_by(**data)[0]
-        return obj
-
-    def update_one(self, filters, fields):
-        obj = self.session.query(self.model).filter_by(**filters).update(fields)
-        self.session.flush()
-        return obj
-
-    def delete_one(self, data: dict) -> int:
-        stmt = delete(self.model)
-        for key, value in data.items():
-            stmt = stmt.where(getattr(self.model, key) == value)
-        result = self.session.execute(stmt)
-        self.session.flush()
-        count = result.rowcount
-        if not count:
-            raise NoResultFound
-        return count
+    def delete_one(self, object_id: int) -> None:
+        pass
